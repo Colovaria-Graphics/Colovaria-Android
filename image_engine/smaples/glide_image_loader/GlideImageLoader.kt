1367a38360da02069171bf3d63e9a0f72f5f367f
Roi Amiel <roiamiel.ra@gmail.com>
2022-10-09 17:18:06 +0300

    fix glide image loader

--- image_engine/smaples/glide_image_loader/GlideImageLoader.kt
+++ image_engine/smaples/glide_image_loader/GlideImageLoader.kt
@@ -1,16 +1,37 @@
 package com.glide_image_loader
 
+import android.graphics.BitmapFactory
+import androidx.exifinterface.media.ExifInterface
+import androidx.exifinterface.media.ExifInterface.*
+import com.bumptech.glide.*
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.Options
+import com.bumptech.glide.load.ResourceDecoder
+import com.bumptech.glide.load.data.DataFetcher
+import com.bumptech.glide.load.data.HttpUrlFetcher
+import com.bumptech.glide.load.data.StreamAssetPathFetcher
+import com.bumptech.glide.load.data.StreamLocalUriFetcher
+import com.bumptech.glide.load.engine.Resource
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.ModelLoader
+import com.bumptech.glide.load.model.ModelLoaderFactory
+import com.bumptech.glide.load.model.MultiModelLoaderFactory
+import com.bumptech.glide.load.model.stream.HttpGlideUrlLoader
+import com.bumptech.glide.load.resource.SimpleResource
+import com.bumptech.glide.load.resource.transcode.ResourceTranscoder
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.signature.ObjectKey
+import com.colovaria.image_engine.api.resources.*
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.io.InputStream
 import android.content.Context
 import android.graphics.Bitmap
 import androidx.collection.LruCache
-import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestBuilder
-import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.FutureTarget
 import com.colovaria.geometry.Size
-import com.colovaria.image_engine.api.resources.ImageLoader
-import com.colovaria.image_engine.api.resources.ResourcePath
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 
@@ -56,4 +77,112 @@ class GlideImageLoader(private val context: Context) : ImageLoader {
     companion object {
         private const val MAX_SIZE_CACHE_SIZE = 10000
     }
+}
+
+class BitmapSizeDecoder : ResourceDecoder<InputStream, BitmapFactory.Options> {
+    override fun handles(inputStream: InputStream, options: Options) : Boolean {
+        return true
+    }
+
+    override fun decode(inputStream: InputStream, width: Int, height: Int, options: Options) : Resource<BitmapFactory.Options> {
+        return SimpleResource(BitmapFactory.Options().apply {
+            inJustDecodeBounds = true
+
+            assert(inputStream.markSupported())
+            inputStream.mark(0)
+
+            BitmapFactory.decodeStream(inputStream, null, this)?.recycle()
+
+            inputStream.reset()
+
+            // Since we skip the glide code that transpose images, we need to do it manually.
+            if (shouldTranspose(inputStream)) {
+                val transposedWidth = outHeight
+                val transposedHeight = outWidth
+                outWidth = transposedWidth
+                outHeight = transposedHeight
+            }
+        })
+    }
+
+    private fun shouldTranspose(inputStream: InputStream) : Boolean {
+        return when (ExifInterface(inputStream).getAttributeInt(TAG_ORIENTATION, ORIENTATION_NORMAL)) {
+            ORIENTATION_ROTATE_90, ORIENTATION_ROTATE_270 -> true
+            else -> false
+        }
+    }
+}
+
+@GlideModule
+class GlideWithImageLoader : AppGlideModule() {
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        registry.prepend(ResourcePath::class.java, InputStream::class.java,
+            ResourcePathLoaderFactory(context)
+        )
+        registry.prepend(InputStream::class.java, BitmapFactory.Options::class.java,
+            BitmapSizeDecoder()
+        )
+        registry.register(BitmapFactory.Options::class.java, Size::class.java,
+            OptionsSizeResourceTranscoder()
+        )
+    }
+}
+
+class OptionsSizeResourceTranscoder : ResourceTranscoder<BitmapFactory.Options, Size> {
+    override fun transcode(resource: Resource<BitmapFactory.Options>, options: Options) : Resource<Size> {
+        return SimpleResource(resource.get().run { Size(outWidth, outHeight) })
+    }
+}
+
+class ResourcePathLoader(
+    private val context: Context,
+) : ModelLoader<ResourcePath, InputStream> {
+    override fun buildLoadData(model: ResourcePath, width: Int, height: Int, options: Options) : ModelLoader.LoadData<InputStream> {
+        return ModelLoader.LoadData(
+            ObjectKey(model), when (model) {
+                is AssetResourcePath -> StreamAssetPathFetcher(context.assets, model.path)
+                is InternalStorageResourcePath -> StreamFilePath(model.path)
+                is ExternalStorageResourcePath -> StreamLocalUriFetcher(context.contentResolver, model.uri)
+                is WebResourcePath -> HttpUrlFetcher(GlideUrl(model.url), options.get(HttpGlideUrlLoader.TIMEOUT) ?: 0)
+                is AndroidResResourcePath -> error("Can't load from android resources")
+            })
+    }
+
+    override fun handles(model: ResourcePath) : Boolean {
+        return true
+    }
+
+    class StreamFilePath(private val path: String) : DataFetcher<InputStream> {
+        private var inputStream: InputStream? = null
+
+        override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in InputStream>) {
+            try {
+                inputStream?.close()
+                inputStream = FileInputStream(path)
+                callback.onDataReady(inputStream)
+            } catch (e: FileNotFoundException) {
+                callback.onLoadFailed(e)
+            }
+        }
+
+        override fun cleanup() {
+            inputStream?.close()
+        }
+
+        override fun cancel() {}
+
+        override fun getDataClass() = InputStream::class.java
+
+        override fun getDataSource() = DataSource.LOCAL
+    }
+}
+
+class ResourcePathLoaderFactory(
+    private val context: Context,
+) : ModelLoaderFactory<ResourcePath, InputStream> {
+    override fun build(multiFactory: MultiModelLoaderFactory): ModelLoader<ResourcePath, InputStream> {
+        return  ResourcePathLoader(context)
+    }
+
+    override fun teardown() {}
 }
