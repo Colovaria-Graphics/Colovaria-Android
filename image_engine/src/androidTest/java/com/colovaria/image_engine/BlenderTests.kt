d174a85083b2735d11fcb7b57efded45e96a6d4b
Roi Amiel <roiamiel.ra@gmail.com>
2022-10-16 00:47:50 +0300

    [ImageEngine] - add more blending tests for blander test.

--- image_engine/src/androidTest/java/com/colovaria/image_engine/BlenderTests.kt
+++ image_engine/src/androidTest/java/com/colovaria/image_engine/BlenderTests.kt
@@ -3,6 +3,7 @@ package com.colovaria.image_engine
 import com.colovaria.geometry.AdjustMode
 import com.colovaria.geometry.Size
 import com.colovaria.geometry.SizeF
+import com.colovaria.geometry.Vector2F
 import com.colovaria.graphics.GFrameBuffer
 import com.colovaria.graphics.GTexture
 import com.colovaria.graphics.egl.GContext
@@ -10,6 +11,7 @@ import com.colovaria.graphics.egl.GDisplay
 import com.colovaria.graphics.egl.GSurface
 import com.colovaria.graphics.uniforms.GColor
 import com.colovaria.graphics.withBind
+import com.colovaria.image_engine.api.blend.BlendMode
 import com.colovaria.image_engine.api.blend.BlenderInstruction
 import com.colovaria.image_engine.api.texture.CircleInstruction
 import com.colovaria.image_engine.api.texture.RectInstruction
@@ -47,6 +49,74 @@ class BlenderTests : TestClass() {
         }
 
         BitmapUtils.assertBitmapEqual(frameBuffer.save(), "test_results/blender/test_1_result.png")
+    }
+
+    @Test
+    fun Blender_DrawCircleOverRectAspectFill_ShouldByAsExpected() {
+        val backTexture = generateRectTexture(SizeF(1f, 1f), GColor.GREEN)
+        val frontTexture = generateCircleTexture(SizeF(0.5f, 0.5f), GColor.RED)
+
+        val frameBuffer = GFrameBuffer(FAKE_CANVAS_SIZE)
+        frameBuffer.withBind {
+            blender.blend(
+                backTexture,
+                frontTexture,
+                BlenderInstruction(center = Vector2F(0.3f, 0.8f), adjustMode = AdjustMode.ASPECT_FILL)
+            )
+        }
+
+        BitmapUtils.assertBitmapEqual(frameBuffer.save(), "test_results/blender/test_2_result.png")
+    }
+
+    @Test
+    fun Blender_DrawCircleOverRectDarken_ShouldByAsExpected() {
+        val backTexture = generateRectTexture(SizeF(1f, 1f), GColor.GREEN)
+        val frontTexture = generateCircleTexture(SizeF(0.5f, 0.5f), GColor.RED)
+
+        val frameBuffer = GFrameBuffer(FAKE_CANVAS_SIZE)
+        frameBuffer.withBind {
+            blender.blend(
+                backTexture,
+                frontTexture,
+                BlenderInstruction(blendMode = BlendMode.DARKEN, adjustMode = AdjustMode.ORIGINAL)
+            )
+        }
+
+        BitmapUtils.assertBitmapEqual(frameBuffer.save(), "test_results/blender/test_3_result.png")
+    }
+
+    @Test
+    fun Blender_DrawCircleOverRectLighten_ShouldByAsExpected() {
+        val backTexture = generateRectTexture(SizeF(1f, 1f), GColor.GREEN)
+        val frontTexture = generateCircleTexture(SizeF(0.5f, 0.5f), GColor.RED)
+
+        val frameBuffer = GFrameBuffer(FAKE_CANVAS_SIZE)
+        frameBuffer.withBind {
+            blender.blend(
+                backTexture,
+                frontTexture,
+                BlenderInstruction(blendMode = BlendMode.LIGHTEN, adjustMode = AdjustMode.ORIGINAL)
+            )
+        }
+
+        BitmapUtils.assertBitmapEqual(frameBuffer.save(), "test_results/blender/test_4_result.png")
+    }
+
+    @Test
+    fun Blender_DrawCircleOverRectMultiply_ShouldByAsExpected() {
+        val backTexture = generateRectTexture(SizeF(1f, 1f), GColor.GREEN)
+        val frontTexture = generateCircleTexture(SizeF(0.5f, 0.5f), GColor(0.5f, 0.5f, 0.5f, 1.0f))
+
+        val frameBuffer = GFrameBuffer(FAKE_CANVAS_SIZE)
+        frameBuffer.withBind {
+            blender.blend(
+                backTexture,
+                frontTexture,
+                BlenderInstruction(scale = Vector2F(0.6f, 0.3f), opacity = 0.9f, blendMode = BlendMode.MULTIPLY, adjustMode = AdjustMode.ORIGINAL)
+            )
+        }
+
+        BitmapUtils.assertBitmapEqual(frameBuffer.save(), "test_results/blender/test_5_result.png")
     }
 
     @After
